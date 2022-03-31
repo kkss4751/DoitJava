8bbc7e31c0823262c37ad6bbd45ea11a5d45e2de
kkss4751 <kkss4751@naver.com>
2022-03-31 16:44:43 +0200

    03/31 단순 for문 연습

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Hello{
     public static void main(String[] args){
-        System.out.println("hello java");
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+
+        for ( a = a;a > 0; a--){
+            System.out.println(a);
+        }
     }
 }
