c51bf4760ce96bb9582a2e5bbcb8f49a1740a4a3
kkss4751 <kkss4751@naver.com>
2022-04-01 06:01:43 +0200

    04/01 기초 while 문 연습

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,12 +2,11 @@ import java.util.Scanner;
 
 public class Hello{
     public static void main(String[] args){
-        int a;
-        Scanner sc = new Scanner(System.in);
-        a = sc.nextInt();
-
-        for ( a = a;a > 0; a--){
-            System.out.println(a);
+        int a = 0;
+        while (a < 10){
+            System.out.println("while 문"+a );
+            a++;
         }
     }
 }
+
