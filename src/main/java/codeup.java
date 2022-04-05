e11c2e64986d3783d29776d4592e0b2bb1602be2
kkss4751 <kkss4751@naver.com>
2022-04-05 15:05:35 +0200

    04/04 code up 1256 [* 출력하기]

---
+++ src/main/java/codeup.java
@@ -0,0 +1,20 @@
+/*
+import java.util.Scanner;
+
+public class Hello{
+    public static void main(String[] args){
+        int a,b,c=0;
+        Scanner sc = new Scanner(System.in);
+        a= sc.nextInt();
+
+        for(b=1; ; b++){
+            c+=b;
+            if ( c>=a)
+                break;
+        }
+        System.out.println(c);
+    }
+}
+``````````````````````````````````````````````````````````````````````
+
+ */
